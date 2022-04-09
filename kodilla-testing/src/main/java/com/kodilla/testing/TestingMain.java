cad38b912456473d6d473586caa0db740be8a901
Robert881009 <robert8810@o2.pl>
2022-04-09 11:32:54 +0200

    6.2 TDD test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,30 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int result1 = calculator.addition(200,100);
+        int result2 = calculator.subtraction(300,100);
+
+        if (result1>0 && result2>0){
+            System.out.println("test OK");
+        }else {
+            System.out.println("Error");
+        }
     }
 }
 
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int result;
+
+    public int addition(int a, int b){
+        result = a + b;
+        return result;
+    }
+    public int subtraction(int a, int b){
+        result = a - b;
+        return result;
+    }
+}
+
